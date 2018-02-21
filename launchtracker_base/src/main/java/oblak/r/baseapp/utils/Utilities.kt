8cc6fc3fee79ba38c7261650c33a645c1c560b91
Rok Oblak <r.oblak@sportradar.com>
2018-02-21 07:29:16 +0100

    Date parsing fix

--- launchtracker_base/src/main/java/oblak/r/baseapp/utils/Utilities.kt
+++ launchtracker_base/src/main/java/oblak/r/baseapp/utils/Utilities.kt
@@ -9,7 +9,7 @@ object ModelConsts {
 
     private const val inputDateFormat: String = "MMMMM d, yyyy HH:mm:ss z"
     private const val outputDateFormat: String = "YYYY.MM.dd"
-    private const val shortCountDownFormat: String = "d'd' h'h' mm'min' ss's'"
+    private const val shortCountDownFormat: String = "h'h' mm'min' ss's'"
 
     // dummy data for when no rockets are selected
     val allRocketObj = DisplayableRocket("All")
@@ -28,5 +28,10 @@ object ModelConsts {
 
     fun formatToRemaining(dateString: String) = countDownFormat.format(remainingDate(dateString))
 
-    fun formatToRemaining(date: Date) = countDownFormat.format(Date(date.time - Date().time))
+    fun formatToRemaining(date: Date): String {
+        // difference in days is broken in this time format for some reason
+        val diff = date.time - Date().time
+        val diffInDays = diff / (3600*1000*24)
+        return "${diffInDays}d ${countDownFormat.format(Date(diff))}"
+    }
 }
